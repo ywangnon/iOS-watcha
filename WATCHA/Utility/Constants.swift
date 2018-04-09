804876c4613cdea1153dbfe7c4278139ad4fb0e3
ywangnon <ywangnon123@gmail.com>
2018-04-09 21:42:26 +0900

    add API

---
+++ WATCHA/Utility/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  WATCHA
+//
+//  Created by Hansub Yoo on 2018. 4. 9..
+//  Copyright © 2018년 Seo Jaehyeong. All rights reserved.
+//
+
+import Foundation
+
+enum API {
+    static let baseURL = "https://justdo2t.com/"
+    
+    enum Auth {
+        static let emailSignUp = API.baseURL + "api/members/signup/"
+        static let emailSignIn = API.baseURL + "api/members/email-auth-token/"
+        static let facebookSignIn = API.baseURL + "api/members/facebook-auth-token/"
+    }
+    enum Post {
+        static let list = API.baseURL + "api/members/"
+    }
+}
