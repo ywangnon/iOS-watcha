d95b7ded6e74f89ea60806787b776827312c7722
caudatus <jaehyeongee@gmail.com>
2018-04-10 16:41:44 +0900

    add RatingModel

---
+++ WATCHA/Model/RatingModel.swift
@@ -0,0 +1,21 @@
+//
+//  RatingModel.swift
+//  WATCHA
+//
+//  Created by Seo JaeHyeong on 10/04/2018.
+//  Copyright © 2018 Seo Jaehyeong. All rights reserved.
+//
+
+import Foundation
+
+struct RatingModel {
+   
+   var movieTitle: String = ""
+   var movieYear: String = ""
+   var movieImage: String = "ready_player_one"
+   var movieRate: Int = 0
+   
+   var moviePk: String = ""
+   var movieCategory: String = ""
+   
+}
