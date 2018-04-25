8ecbd73ee0f4a75b2456a0ba8bab178aa00c6839
HimchanPark <ting1099@gmail.com>
2018-04-25 15:33:57 +0900

    update

--- WATCHA/Model/ThemeDetail.swift
+++ WATCHA/Model/ThemeDetail.swift
@@ -8,46 +8,25 @@
 
 import Foundation
 
-struct ThemeMovieList: Codable {
-    //영화 리스트 갯수
-    var count: Int
-    //다음 페이지 링크 url
-    var next: String?
-    //이전 페이지 링크 url
-    var previous: String?
-    //영화 정보 리스트
-    var results: [ThemeMovie]
+struct ThemeDetailList {
+    var poster: String
+    var title: String
+    var averageRate: String
+    var ticketingRate: String
+    var director: String
+    var actors: String
 }
 
-struct ThemeMovie: Codable{
-    
-    //영화 제목
-    var title: String
-    //출시연도
-    var year: Int
-    //영화 포스터 이미지 링크 - 웹
-    var posterImageWeb: String
-    //영화 포스터 이미지 링크 - 앱
-    var posterImage: String
-    //영화 평균 별점
-    var averageRate: String
-    //영화 id
-    var pk: Int
-    //영화 카테고리(장르)
-    var genre: [Genre]
-    //영화 태그
-    //var tag: [Int]
-    
-    
-    private enum CodingKeys: String, CodingKey {
-        case title = "title_ko"
-        case year = "movie_created_date"
-        case posterImageWeb = "poster_image_m"
-        case posterImage = "poster_image_eval_x3"
-        case averageRate = "rating_avg"
-        case pk = "id"
-        case genre
-        //case tag
-    }
-    
-}
+
+
+let movie1 = ThemeDetailList(poster: "movie1Poster", title: "어벤져스: 인피니티 워", averageRate: "4.4", ticketingRate: "96%", director: "루소 형제", actors: "로버트 다우니 주니어, 조쉬 브롤린, 스칼렛 요한슨")
+
+let movie2 = ThemeDetailList(poster: "movie2Poster", title: "그날 바다", averageRate: "4.9", ticketingRate: "0.4%", director: "김지영", actors: "정우성")
+
+let movie3 = ThemeDetailList(poster: "movie3Poster", title: "램페이지", averageRate: "3.1", ticketingRate: "0.19%", director: "브레드 페이튼", actors: "드웨인 존슨, 나오미 헤리스")
+
+let movie4 = ThemeDetailList(poster: "movie4Poster", title: "콰이어트 플레이스", averageRate: "3.5", ticketingRate: "0.17%", director: "존 크래진스키", actors: "에밀리 블런튼, 존 크래진스키")
+
+let movie5 = ThemeDetailList(poster: "movie5Poster", title: "얼리맨", averageRate: "3.1", ticketingRate: "0.05", director: "닉 파크", actors: "에디 레드메인, 톰 히들스턴")
+
+let movies = [movie1, movie2, movie3, movie4, movie5]
